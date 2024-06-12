3f5916a38ac5c47d04a46881c6a6650c25bba35f
21wh1a6612 <96616388+21wh1a6612@users.noreply.github.com>
2024-06-12 09:35:22 +0530

    Update project.java

--- project.java
+++ project.java
@@ -1,19 +1,6 @@
-import java.util.Scanner;
-
-public class project{
-    public static void main(String[] args) {
-        // Create a Scanner object to read input
-        Scanner scanner = new Scanner(System.in);
-        
-        // Prompt the user to enter their name
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        
-        // Display the greeting message
-        System.out.println("Hello, " + name + "! Welcome to the Java program.");
-        
-        // Close the scanner
-        scanner.close();
-    }
+class project{
+	public static void main(String args[])
+	{
+		System.out.println("Hello, World");
+	}
 }
-
